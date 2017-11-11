6f6afd67a8ea20d9ede246a56a5efe574a2f81e9
Manuel <zhindonm@gmail.com>
2017-11-11 14:29:52 +0000

    Added test to post hello world

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
+from secret import ACCESS_SECRET, ACCESS_TOKEN, CONSUMER_KEY, CONSUMER_SECRET
 import os
-from markovbot import markovbot
+#from markovbot import MarkovBot
+import tweepy
+import random
 
-#Initialize a markov bot instance
-tweetbot = MarkovBot()
+def setTwitterAuth():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    return api
+
+def tweetHelloWorld(api):
+    api.update_status("Hello, World! #{}"
+                      .format(random.randint(0, 10000)))
+
+
+if __name__ == "__main__":
+    api = setTwitterAuth()
+    tweetHelloWorld(api)
