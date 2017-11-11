bedfd51818eebc4b09caee433977dfb18223e95b
Nikolina Tudjarova <ninna.archer@gmail.com>
2017-11-11 23:33:34 +0000

    Add files via upload

---
+++ stringReplacement.py
@@ -0,0 +1,12 @@
+#import string
+def replace_string(tweet);
+	old_words = ['witnesses', 'new study', 'Trump', 'Donald Trump', 'murder', 'cash', 'war', 'president' ]
+	new_words = ['the dudes I do not know', 'new reddit post', 'worst president ever', 'not happy ending', 'notes', 'game of the smart animals', 'the lord']
+	
+	for word in tweet:
+		if word in
+    new_string = tweet.replace('witnesses', 'the dudes I do not know')
+	new_string = tweet.replace('new study', 'new reddit post')
+	new_string = tweet.replace('Trump|Donald Trump', 'worst president ever')
+	new_string = tweet.replace('murder', 'not happy ending')
+	new_string = tweet.replace('cash', 'notes')
