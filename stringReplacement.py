ac769bc43ca74d5ed820b8f597d80b24918f51a3
Humza M shaheen <anjum.humza@gmail.com>
2017-11-12 09:41:40 +0000

    asd

--- stringReplacement.py
+++ stringReplacement.py
@@ -1,2 +1,31 @@
-def filter(text):
-	return texts
+def filter(tweet):
+	tweet = tweet.split()
+	for i in range(0, len(tweet) - 1):
+		if tweet[i] == "witnesses":
+			tweet[i] = "these dudes i know"
+
+		elif tweet[i] == "allegedly":
+			tweet[i] = "kinda probably"
+			tweet[i] = "avenge"
+
+		elif tweet[i] == "space":
+			tweet[i] = "spaaqce"
+
+		elif tweet[i] == "smartphone":
+			tweet[i] = "pokedex"
+
+		elif tweet[i] == "electric":
+			tweet[i] = "atomic"
+
+		elif tweet[i] == "car":
+			tweet[i] = "cat"
+
+		elif tweet[i] == "election":
+			tweet[i] = "eating contest"
+
+		elif tweet[i] == "car":
+			tweet[i] = "cat"
+		elif tweet[i] == "senator":
+			tweet[i] = "elf-lord"
+
+	return " ".join(tweet)
